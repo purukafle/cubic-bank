2034839e84cd2843603339d75b2db46ebf4914e4
JavaHunk2020 <javahunk2020@gmail.com>
2021-07-23 22:26:10 +0530

    code for loan crud

--- banking-web/src/main/java/com/rab3tech/SecurityConfiguration.java
+++ banking-web/src/main/java/com/rab3tech/SecurityConfiguration.java
@@ -46,6 +46,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   @Override 
   protected void configure(HttpSecurity http) throws Exception {   
     http.authorizeRequests() 
+        .antMatchers("/admin/loantypes").permitAll() 
+        .antMatchers("/admin/dloantype").permitAll()
         .antMatchers("/admin/loantype").permitAll() 
         .antMatchers("/admin/addBranch").permitAll() 
         .antMatchers("/admin/deleteBranch").permitAll() 
--- banking-web/src/main/java/com/rab3tech/admin/ui/controller/LoanTypesUIController.java
+++ banking-web/src/main/java/com/rab3tech/admin/ui/controller/LoanTypesUIController.java
@@ -2,6 +2,7 @@ package com.rab3tech.admin.ui.controller;
 
 import java.sql.Timestamp;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
@@ -11,19 +12,38 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.rab3tech.admin.service.LoanTypesService;
 import com.rab3tech.vo.LoanTypesVO;
 
 @Controller
 @RequestMapping("/admin")
-@Scope("singleton")
+@Scope("singleton")  //we have seven scopes
 //@CrossOrigin()
 public class LoanTypesUIController {
 	
 	
 	@Autowired
 	private LoanTypesService loanTypesService;
+	
+	
+	@GetMapping("/loantypes")
+	public String showuploadLoanTypes(Model model) {
+		List<LoanTypesVO> loantypeVOs=loanTypesService.getAll();
+		//model is used to send data from controller to view (thyme leaf page)
+		model.addAttribute("loantypeVOs",loantypeVOs);
+		//model is used to send data from controller to UI
+		return "admin/loantypes"; 
+	}
+	
+	//dloantype ? id =222
+	@GetMapping("/dloantype")
+	public String deleteLoanType(@RequestParam int id,Model model) {
+		loanTypesService.deleteData(id);
+		//below line will hit controller again to show the data
+		return "redirect:/admin/loantypes"; 
+	}
 	
 	
 	@GetMapping("/loantype")
